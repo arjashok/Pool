97aa77ac8310ee2b578d97be19fa0d91d3c2bd15
Yashwanth Ravipati <yashravipatischool@gmail.com>
2023-07-31 19:40:31 -0700

    heo

--- algorithms/noah_clustering.py
+++ algorithms/noah_clustering.py
@@ -8,21 +8,28 @@ from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 
-coords = np.array([(0,0), (1,1), (1,0), (6,0), (8,0), (7,1), (5,4), (4,5), (1,2)])
+coords = np.array(
+    [(0, 0), (1, 1), (1, 0), (6, 0), (8, 0), (7, 1), (5, 4), (4, 5), (1, 2)]
+)
 
 
 def get_even_clusters(X, cluster_size):
-    n_clusters = int(np.ceil(len(X)/cluster_size))
+    n_clusters = int(np.ceil(len(X) / cluster_size))
     kmeans = KMeans(n_clusters)
     kmeans.fit(X)
     centers = kmeans.cluster_centers_
-    centers = centers.reshape(-1, 1, X.shape[-1]).repeat(cluster_size, 1).reshape(-1, X.shape[-1])
+    centers = (
+        centers.reshape(-1, 1, X.shape[-1])
+        .repeat(cluster_size, 1)
+        .reshape(-1, X.shape[-1])
+    )
     distance_matrix = cdist(X, centers)
-    clusters = linear_sum_assignment(distance_matrix)[1]//cluster_size
+    clusters = linear_sum_assignment(distance_matrix)[1] // cluster_size
     return clusters
 
+
 def group(arr, coords, cluster_size):
-    num_clusters = int(np.ceil(len(coords)/cluster_size))
+    num_clusters = int(np.ceil(len(coords) / cluster_size))
     rv = []
     for i in range(num_clusters):
         rv.append([])
@@ -31,6 +38,7 @@ def group(arr, coords, cluster_size):
         rv[i].append(list(coords[idx]))
         idx += 1
     return rv
+
 
 print(coords)
 check = get_even_clusters(coords, 3)
