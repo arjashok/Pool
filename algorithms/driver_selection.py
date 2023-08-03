3dad829bf544bbb48cf6daa0cbc2fe84627b7a60
Arjun Ashok <arjun3.ashok@gmail.com>
2023-08-03 11:36:04 -0700

    distance file for segmenting all code for distance calculations

--- algorithms/driver_selection.py
+++ algorithms/driver_selection.py
@@ -28,10 +28,9 @@ from sklearn.cluster import KMeans                  # clustering
 from scipy.optimize import linear_sum_assignment    # magic function lol
 import numpy as np                                  # arrays
 import itertools                                    # iteration
+from distance import *                              # all distance functionality
 
 # temporary, testing
-import math                                         # distance
-from scipy.spatial.distance import cdist            # Euclidean distance
 import matplotlib.pyplot as plt                     # visualizing
 import pandas as pd                                 # dataframe
 from cost_testing import *                          # testing purposes
@@ -45,57 +44,8 @@ from corporate_clustering import *                  # clustering help
 # ------ TSP Algo ------ #
 
 
-# ------ Auxiliary Functions for Distance ------ #
-"""
-    Temporary distance formula that relies on direct distance rather than time
-    to drive the route between waypoints.
 
-    This will eventually be replaced with a Google Maps API call, but for now
-    we use this formula.
-"""
-def distance(p1: tuple, p2: tuple) -> float:
-    # return Euclidean distance #
-    return math.dist(p1, p2)
-
-
-"""
-    Auxiliary function to allow for triangular-matrix space optimization. Since
-    variables are auto passed by reference in python unless acted on by a
-    non-in-place manner, we can assume some efficiency.
-"""
-def get_dist(coordinates: np.ndarray, row: int, col: int) -> float:
-    # check direct access
-    if row >= col:
-        return coordinates[row, col]
-    
-    # indirect access
-    return coordinates[col, row]
-
-
-"""
-    Assumed non-symmetry for distance (i.e weight, time between waypoints) to
-    account for differing traffic levels, closures, etc. in the future. API
-    calls will be n(n - 1) ==> O(n^2), but with symmetry they would become
-    n(n - 1) / 2 ==> O(n^2).
-
-    UPDATE: we are now switching to a symmetric approach to ensure undirected
-    edges when clustering and finding optimal driver
-"""
-def create_distance_matrix(coordinates: np.ndarray) -> np.ndarray:
-    # setup
-    n = coordinates.shape[0]
-    matrix = [[0] * i for i in range(1, n + 1)]
-
-    # get all distances
-    for i in range(n):
-        for j in range(0, i):
-            dist = distance(coordinates[i], coordinates[j])
-            matrix[i, j] = dist
-
-    # return 2D
-    return np.array(matrix)
-
-
+# ------ Auxiliary Functions for Selection ------ #
 """
     Given a narrowed dataframe of only the relevant driver rows, this utility
     calculates the final driver weights according to the formula we have custom
@@ -121,8 +71,9 @@ def driver_weights(driver_data: pd.DataFrame) -> np.ndarray:
     alpha = np.array([ALPHA] * DRIVERS)                 # constant multiplier
     gas_mileage = 1 / driver_data["gas_mileage"]        # miles per gallon
     preference = 1 / driver_data["pref_to_drive"]       # driving preference
-    exhaustion = (driver_data["trips_driven"]) / (
-        driver_data["trips_taken"] + 1
+    exhaustion = (driver_data["trips_driven"] 
+                  + driver_data["miles_driven"])
+                  / (driver_data["trips_taken"] + 1
     )                                                   # driving instances / num trips + 1
     rsvp_time = (
         1 / driver_data["rsvp_time"]
---
+++ distance.py
@@ -0,0 +1,64 @@
+"""
+    Utility file for all functions relating to distance and distance
+    calculations.
+"""
+
+
+# ------ Environment Setup ------ #
+# static libaries
+import numpy as np                  # array manipulation
+
+# temporary libraries
+import math                         # distance calculations
+
+
+# ------ Auxiliary Functions for Distance ------ #
+"""
+    Temporary distance formula that relies on direct distance rather than time
+    to drive the route between waypoints.
+
+    This will eventually be replaced with a Google Maps API call, but for now
+    we use this formula.
+"""
+def distance(p1: tuple, p2: tuple) -> float:
+    # return Euclidean distance #
+    return math.dist(p1, p2)
+
+
+"""
+    Auxiliary function to allow for triangular-matrix space optimization. Since
+    variables are auto passed by reference in python unless acted on by a
+    non-in-place manner, we can assume some efficiency.
+"""
+def get_dist(coordinates: np.ndarray, row: int, col: int) -> float:
+    # check direct access
+    if row >= col:
+        return coordinates[row, col]
+    
+    # indirect access
+    return coordinates[col, row]
+
+
+"""
+    Assumed non-symmetry for distance (i.e weight, time between waypoints) to
+    account for differing traffic levels, closures, etc. in the future. API
+    calls will be n(n - 1) ==> O(n^2), but with symmetry they would become
+    n(n - 1) / 2 ==> O(n^2).
+
+    UPDATE: we are now switching to a symmetric approach to ensure undirected
+    edges when clustering and finding optimal driver
+"""
+def create_distance_matrix(coordinates: np.ndarray) -> np.ndarray:
+    # setup
+    n = coordinates.shape[0]
+    matrix = [[0] * i for i in range(1, n + 1)]
+
+    # get all distances
+    for i in range(n):
+        for j in range(0, i):
+            dist = distance(coordinates[i], coordinates[j])
+            matrix[i, j] = dist
+
+    # return 2D
+    return np.array(matrix)
+
