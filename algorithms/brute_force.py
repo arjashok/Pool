65bf4907d62ad51ba87aed7da0b03e49086f7338
noahantisseril <86316131+noahantisseril@users.noreply.github.com>
2023-08-02 21:20:19 -0700

    caches data on runs

--- algorithms/brute_force.py
+++ algorithms/brute_force.py
@@ -8,20 +8,26 @@ import time
 def distance(p1, p2):
     return math.dist(p1, p2)
 
-def find_path_distance(arr):
+def find_path_distance(arr, cache):
     rv = 0
     for i in range(len(arr) - 1):
-        rv += distance(arr[i], arr[i + 1])
+        if (arr[i], arr[i + 1]) not in cache:
+            dist = distance(arr[i], arr[i + 1])
+            cache[(arr[i], arr[i + 1])] = dist
+            rv += dist
+        else:
+            rv += cache[(arr[i], arr[i + 1])]
     return rv
 
 def brute_force(coordinates):
+    cache = {}
     permutations = itertools.permutations(coordinates[:-1])
     min_distance = float('inf')
     optimal_path = []
     for path in permutations:
         path = list(path)
         path.append(coordinates[len(coordinates) - 1])
-        curr_distance = find_path_distance(path)
+        curr_distance = find_path_distance(path, cache)
         if curr_distance < min_distance:
             min_distance = curr_distance
             optimal_path = path
@@ -29,9 +35,8 @@ def brute_force(coordinates):
 
 if __name__ == "__main__":
     start_time = time.time()
-    coords = rand_coordinates(5)
-    #print(np.array(coords))
+    coords = [(5,6), (2,5), (3,10), (11,2), (7,4), (8,3), (5,5), (9,9), (0,11), (0,0)]
     path = brute_force(coords)
-    #print(np.array(path))
-    #visualize_path(path)
+    #print(path)
+    visualize_path(path)
     print("--- %s seconds ---" % (time.time() - start_time))
