5dafd257d2ae6e8fe7d4883fe77b1130a46dce84
noahantisseril <86316131+noahantisseril@users.noreply.github.com>
2023-08-02 20:11:28 -0700

    dont specify start, time runtime

--- algorithms/brute_force.py
+++ algorithms/brute_force.py
@@ -3,6 +3,7 @@ from cost_testing import *
 import matplotlib.pyplot as plt
 import numpy as np
 import itertools
+import time
 
 def distance(p1, p2):
     return math.dist(p1, p2)
@@ -14,13 +15,12 @@ def find_path_distance(arr):
     return rv
 
 def brute_force(coordinates):
-    permutations = itertools.permutations(coordinates[1:-1])
+    permutations = itertools.permutations(coordinates[:-1])
     min_distance = float('inf')
     optimal_path = []
     for path in permutations:
         path = list(path)
         path.append(coordinates[len(coordinates) - 1])
-        path.insert(0, coordinates[0])
         curr_distance = find_path_distance(path)
         if curr_distance < min_distance:
             min_distance = curr_distance
@@ -28,8 +28,10 @@ def brute_force(coordinates):
     return optimal_path
 
 if __name__ == "__main__":
+    start_time = time.time()
     coords = rand_coordinates(5)
-    print(np.array(coords))
+    #print(np.array(coords))
     path = brute_force(coords)
-    print(np.array(path))
-    visualize_path(path)
+    #print(np.array(path))
+    #visualize_path(path)
+    print("--- %s seconds ---" % (time.time() - start_time))
