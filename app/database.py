e83763b529a045023011fd6481d082213c4b8833
Yashwanth Ravipati <yashravipatischool@gmail.com>
2023-08-09 03:05:25 -0700

    Database modification features

--- app/database.py
+++ app/database.py
@@ -16,6 +16,7 @@
 
 # ----- Environment setup ----- #
 # static libraries
+from bson import ObjectId
 import numpy as np  # array manipulation
 
 # temporary
@@ -27,26 +28,11 @@ from config import *  # API keys
 
 
 # ------ Auxiliary Functions for Queries ------ #
-"""
-    Functionality to query an organization's id based on name
-"""
-
-
-def db_query_org_id(name: str) -> str:
-    # setup #
-    db = db_load("PoolData")
-    collection_name = db["Organizations"]
-    org_id = collection_name.find_one({"name": name})["_id"]
-
-    # query #
-    return org_id
 
 
 """
     Get user info for a specified list of users using their designated ID.
 """
-
-
 def db_query_users(users: np.ndarray) -> pd.DataFrame:
     # setup #
     db = db_load()
@@ -61,7 +47,7 @@ def db_query_users(users: np.ndarray) -> pd.DataFrame:
     columns will be modified to match the inputted dataframe. The user-ids will
     be matched as primary key to accomplish this.
 """
-def db_modify_users(users: pd.DataFrame) -> None:
+def db_modify_user_pandas(users: pd.DataFrame) -> None:
     # check missing
     if not "user-id" in users.columns:
         print("Failed db update for failed column primary key `user-id`")
@@ -74,6 +60,44 @@ def db_modify_users(users: pd.DataFrame) -> None:
 
     return
 
+def db_modify_user(userID: str, fields: dict) -> None:
+    db = db_load("PoolData")
+    collection = db["Users"]
+    
+    filter = {"_id": ObjectId(userID)}
+    update = {"$set": fields}
+    try:
+        result = collection.update_one(filter, update)
+        print(result.modified_count, "documents updated.")
+    except Exception as e:
+        print("Failed to update user: ", e)
+    return None
+
+def db_modify_event(eventID: str, fields: dict) -> None:
+    db = db_load("PoolData")
+    collection = db["Events"]
+    
+    filter = {"_id": ObjectId(eventID)}
+    update = {"$set": fields}
+    try:
+        result = collection.update_one(filter, update)
+        print(result.modified_count, "documents updated.")
+    except Exception as e:
+        print("Failed to update user: ", e)
+    return None
+
+def db_modify_org(orgID: str, fields: dict) -> None:
+    db = db_load("PoolData")
+    collection = db["Organizations"]
+    
+    filter = {"_id": ObjectId(orgID)}
+    update = {"$set": fields}
+    try:
+        result = collection.update_one(filter, update)
+        print(result.modified_count, "documents updated.")
+    except Exception as e:
+        print("Failed to update user: ", e)
+    return None
 
 # ------ Auxiliary Functions for DB Functionality ------ #
 """
@@ -106,26 +130,35 @@ def db_save(pool_db: pd.DataFrame) -> None:
 """
     Functionality to insert a new organization into the database.
 """
-def db_create_org(name, adminIDs) -> None:
+def db_create_org(name: str, adminIDs: list) -> None:
     # setup #
     db = db_load("PoolData")
     collection_name = db["Organizations"]
     # insert #
-    collection_name.insert_one({"name": name.lower(), "admin-ids": adminIDs})
+    adminIDs = [ObjectId(str_id) for str_id in adminIDs]
+    try:
+        result = collection_name.insert_one({"name": name.lower(), "adminIDs": adminIDs})
+        print(result.inserted_id, "documents inserted.")
+    except Exception as e:
+        print("Failed to insert organization", e)
     return None
 
 
 """
     Insert a user into the database.
 """
-def db_create_user(name, email, address) -> None:
+def db_create_user(name: str, email: str, address: str) -> None:
     # setup #
     db = db_load("PoolData")
     collection_name = db["Users"]
-    geo_code = get_geo(address)
+    try:
+        geo_code = get_geo(address)
+    except Exception as e:
+        print("Could not find geo code for address")
+        return None
     # insert #
     try:
-        collection_name.insert_one(
+        result = collection_name.insert_one(
             {
                 "name": name.lower(),
                 "email": email.lower(),
@@ -133,20 +166,31 @@ def db_create_user(name, email, address) -> None:
                 "geo": geo_code,
             }
         )
-    except:
-        print("Failed to insert user into database")
+        print(result.inserted_id, "documents inserted.")
+    except Exception as e:
+        print("Failed to insert user", e)
     return None
 
-def db_create_event(name, orgID, location):
+def db_create_event(name: str, orgID: str, location: str) -> None:
     # setup #
     db = db_load("PoolData")
     collection_name = db["Events"]
+    try:
+        geo = get_geo(location)
+    except Exception as e:
+        print("Could not find geo code for address")
+        return None
     # insert #
-    collection_name.insert_one(
-        {
-            "name": name.lower(),
-            "org-id": orgID,
-            "location": location.lower(),
-        }
-    )
+    try:
+        result = collection_name.insert_one(
+            {
+                "name": name.lower(),
+                "orgID": ObjectId(orgID),
+                "location": location.lower(),
+                "geo": geo,
+            }
+        )
+        print(result.inserted_id, "documents inserted.")
+    except Exception as e:
+        print("Failed to insert event: ", e)
     return None
