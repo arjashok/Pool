27a767caef27c9e244209e53fdff2a626df2ad82
Yashwanth Ravipati <yashravipatischool@gmail.com>
2023-08-10 02:15:44 -0700

    Adding firebase implementation to user creation

--- app/database.py
+++ app/database.py
@@ -25,6 +25,7 @@ import pandas as pd
 from pymongo import MongoClient  # database
 from MapsAPI import *  # maps API
 from config import *  # API keys
+from firebase_admin import auth, credentials, initialize_app
 
 
 # ------ Auxiliary Functions for Queries ------ #
@@ -60,13 +61,14 @@ def db_modify_user_pandas(users: pd.DataFrame) -> None:
 
     return
 
+
 """
     Modify one of the user entries in the database.
 """
 def db_modify_user(userID: str, fields: dict) -> None:
     db = db_load("PoolData")
     collection = db["Users"]
-    
+
     filter = {"_id": ObjectId(userID)}
     update = {"$set": fields}
     try:
@@ -76,13 +78,14 @@ def db_modify_user(userID: str, fields: dict) -> None:
         print("Failed to update user: ", e)
     return None
 
+
 """
     Modify one of the event entries in the database.
 """
 def db_modify_event(eventID: str, fields: dict) -> None:
     db = db_load("PoolData")
     collection = db["Events"]
-    
+
     filter = {"_id": ObjectId(eventID)}
     update = {"$set": fields}
     try:
@@ -92,13 +95,14 @@ def db_modify_event(eventID: str, fields: dict) -> None:
         print("Failed to update user: ", e)
     return None
 
+
 """
     Modify one of the organization entries in the database.
 """
 def db_modify_org(orgID: str, fields: dict) -> None:
     db = db_load("PoolData")
     collection = db["Organizations"]
-    
+
     filter = {"_id": ObjectId(orgID)}
     update = {"$set": fields}
     try:
@@ -107,6 +111,7 @@ def db_modify_org(orgID: str, fields: dict) -> None:
     except Exception as e:
         print("Failed to update user: ", e)
     return None
+
 
 # ------ Auxiliary Functions for DB Functionality ------ #
 """
@@ -146,19 +151,24 @@ def db_create_org(name: str, adminIDs: list) -> None:
     # insert #
     adminIDs = [ObjectId(str_id) for str_id in adminIDs]
     try:
-        result = collection_name.insert_one({"name": name.lower(), "adminIDs": adminIDs})
+        result = collection_name.insert_one(
+            {"name": name.lower(), "adminIDs": adminIDs}
+        )
         print(result.inserted_id, "documents inserted.")
     except Exception as e:
         print("Failed to insert organization", e)
     return None
 
+
 def db_create_org_pd(entry: pd.DataFrame) -> None:
     db = db_load("PoolData")
     collection_name = db["Organizations"]
     # insert #
-    adminIDs = [ObjectId(str_id) for str_id in entry['adminIDs']]
+    adminIDs = [ObjectId(str_id) for str_id in entry["adminIDs"]]
     try:
-        result = collection_name.insert_one({"name": entry['name'].lower(), "adminIDs": adminIDs})
+        result = collection_name.insert_one(
+            {"name": entry["name"].lower(), "adminIDs": adminIDs}
+        )
         print(result.inserted_id, "documents inserted.")
     except Exception as e:
         print("Failed to insert organization", e)
@@ -168,10 +178,12 @@ def db_create_org_pd(entry: pd.DataFrame) -> None:
 """
     Insert a user into the database.
 """
-def db_create_user(name: str, email: str, address: str) -> None:
+def db_create_user(name: str, email: str, address: str, password: str) -> None:
     # setup #
     db = db_load("PoolData")
     collection_name = db["Users"]
+    cred = credentials.Certificate("firebase_key.json")
+
     try:
         geo_code = get_geo(address)
     except Exception as e:
@@ -189,12 +201,27 @@ def db_create_user(name: str, email: str, address: str) -> None:
         )
         print(result.inserted_id, "documents inserted.")
     except Exception as e:
-        print("Failed to insert user", e)
+        print("Failed to insert user to MongoDB", e)
+        
+    try:
+        firebase_app = initialize_app(cred)
+        user = auth.create_user(
+            email=email,
+            email_verified=False,
+            password=password,
+            display_name=name,
+            disabled=False
+        )
+        print('Sucessfully created new user: {0}'.format(user.uid))
+    except Exception as e:
+        print("Failed to insert user to Firebase", e)
     return None
+
 
 def db_create_user_pd(entry: pd.DataFrame) -> None:
     db = db_load("PoolData")
     collection_name = db["Users"]
+    cred = credentials.Certificate("firebase_key.json")
     try:
         geo_code = get_geo(entry["address"])
     except Exception as e:
@@ -204,8 +231,8 @@ def db_create_user_pd(entry: pd.DataFrame) -> None:
     try:
         result = collection_name.insert_one(
             {
-                "name": entry['name'].lower(),
-                "email": entry['email'].lower(),
+                "name": entry["name"].lower(),
+                "email": entry["email"].lower(),
                 "address": entry["address"].lower(),
                 "geo": geo_code,
             }
@@ -213,7 +240,21 @@ def db_create_user_pd(entry: pd.DataFrame) -> None:
         print(result.inserted_id, "documents inserted.")
     except Exception as e:
         print("Failed to insert user", e)
+        
+    try:
+        firebase_app = initialize_app(cred)
+        user = auth.create_user(
+            email=entry["email"],
+            email_verified=False,
+            password=entry["password"],
+            display_name=entry["name"],
+            disabled=False
+        )
+        print('Sucessfully created new user: {0}'.format(user.uid))
+    except Exception as e:
+        print("Failed to insert user to Firebase", e)
     return None
+
 
 """
     Insert an event into the database.
@@ -242,6 +283,7 @@ def db_create_event(name: str, orgID: str, location: str) -> None:
         print("Failed to insert event: ", e)
     return None
 
+
 def db_create_event_pd(entry: pd.DataFrame) -> None:
     # setup #
     db = db_load("PoolData")
@@ -255,8 +297,8 @@ def db_create_event_pd(entry: pd.DataFrame) -> None:
     try:
         result = collection_name.insert_one(
             {
-                "name": entry['name'].lower(),
-                "orgID": ObjectId(entry['orgID']),
+                "name": entry["name"].lower(),
+                "orgID": ObjectId(entry["orgID"]),
                 "location": entry["location"].lower(),
                 "geo": geo,
             }
@@ -265,3 +307,5 @@ def db_create_event_pd(entry: pd.DataFrame) -> None:
     except Exception as e:
         print("Failed to insert event: ", e)
     return None
+
+db_create_user("Noah", "bob@gmail.com", "Fitness 19 Dublin", "password1234")
